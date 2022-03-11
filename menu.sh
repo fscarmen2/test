e20a7ecae76fa6752b87d1e08211d6fceaa60bee
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-03-11 16:46:11 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -9,7 +9,7 @@ declare -A T
 
 T[E0]="\n Language:\n  1.English (default) \n  2.简体中文\n"
 T[C0]="${T[E0]}"
-T[E1]="1.Open TUN for OVZ; You needn't setting it in the control panel; 2. WARP Client support Ubuntu 18.04 and CentOS 7"
+T[E1]="1.Open TUN for OVZ. You needn't setting it in the control panel; 2. WARP Client support Ubuntu 18.04 and CentOS 7. "
 T[C1]="1.为 OVZ VPS 在线打开 TUN,不需要到面板处理; 2. WARP Client 支持 Ubuntu 18.04 and CentOS 7"
 T[E2]="The script must be run as root, you can enter sudo -i and then download and run again. Feedback: [https://github.com/fscarmen/warp/issues]"
 T[C2]="必须以root方式运行脚本，可以输入 sudo -i 后重新下载运行，问题反馈:[https://github.com/fscarmen/warp/issues]"
