46c46cdb4a0971827cd94781d6c6c9836cc6ffcb
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-04-09 17:51:08 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -611,16 +611,16 @@ change_ip(){
 			done
 
 		else	interface_restart(){
-				red " $(eval echo "${T[${L}126]}") " &&	warp-cli --accept-tos delete >/dev/null 2>&1 && warp-cli --accept-tos register >/dev/null 2>&1 && sleep $j &&
+				red " $(eval echo "${T[${L}126]}") " &&	warp-cli --accept-tos delete >/dev/null 2>&1 && warp-cli --accept-tos register >/dev/null 2>&1 &&
 				[[ -e /etc/wireguard/license ]] && warp-cli --accept-tos set-license $(cat /etc/wireguard/license) >/dev/null 2>&1 && sleep 2
 				warp-cli --accept-tos disconnect >/dev/null 2>&1
 				warp-cli --accept-tos disable-always-on >/dev/null 2>&1
 				ip -4 rule delete from 172.16.0.2 lookup 51820
 				ip -4 rule delete table main suppress_prefixlength 0
-				sleep 2
+				sleep 4
 				warp-cli --accept-tos connect >/dev/null 2>&1
 				warp-cli --accept-tos enable-always-on >/dev/null 2>&1
-				sleep 5
+				sleep 8
 				ip -4 rule add from 172.16.0.2 lookup 51820
 				ip -4 route add default dev CloudflareWARP table 51820
 				ip -4 rule add table main suppress_prefixlength 0
