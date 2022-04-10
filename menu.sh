3b66e84692fb8c7500d3bf688b7179a1fcbdc7f3
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-04-10 09:23:14 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -612,13 +612,13 @@ change_ip(){
 
 		else	interface_restart(){
 				red " $(eval echo "${T[${L}126]}") " &&	warp-cli --accept-tos delete >/dev/null 2>&1 && warp-cli --accept-tos register >/dev/null 2>&1 &&
-				[[ -e /etc/wireguard/license ]] && warp-cli --accept-tos set-license $(cat /etc/wireguard/license) >/dev/null 2>&1 && sleep 2
-				systemctl restart warp-svc
+				[[ -e /etc/wireguard/license ]] && warp-cli --accept-tos set-license $(cat /etc/wireguard/license) >/dev/null 2>&1
+				systemctl restart warp-svc && sleep $j
 				}
 			
 			INTERFACE='--interface CloudflareWARP'
 			[[ -z "$EXPECT" ]] && input_region
-			i=0; j=10
+			i=0; j=5
 			while true
 			do (( i++ )) || true
 			ip_now=$(date +%s); RUNTIME=$((ip_now - ip_start)); DAY=$(( RUNTIME / 86400 )); HOUR=$(( (RUNTIME % 86400 ) / 3600 )); MIN=$(( (RUNTIME % 86400 % 3600) / 60 )); SEC=$(( RUNTIME % 86400 % 3600 % 60 ))
