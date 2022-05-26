def3fd9fd4f439b11be878a2fbbe49ccc8748e5e
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-05-27 01:48:44 +0800

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -195,8 +195,8 @@ net(){
 			fi
         	done
 	green " ${L[26]} "
-	[ $LANGUAGE = 2 ] && COUNTRY4=$(translate "$COUNTRY4")
-	[ $LANGUAGE = 2 ] && COUNTRY6=$(translate "$COUNTRY6")
+	[[ $LANGUAGE = 2 ]] && COUNTRY4=$(translate "$COUNTRY4")
+	[[ $LANGUAGE = 2 ]] && COUNTRY6=$(translate "$COUNTRY6")
 	[[ $OPTION = [on] ]] && green " IPv4:$WAN4 $WARPSTATUS4 $COUNTRY4 $ASNORG4\n IPv6:$WAN6 $WARPSTATUS6 $COUNTRY6 $ASNORG6 "
 }
 
@@ -290,7 +290,7 @@ install(){
 	sudo mv -f /usr/local/bin/mac.sh /etc/wireguard >/dev/null 2>&1
 	sudo ln -sf /etc/wireguard/mac.sh /usr/local/bin/warp && green " ${L[27]} "
 	sudo chmod +x /usr/local/bin/warp
-	[ $LANGUAGE = 2 ] && echo "C" | sudo tee /etc/wireguard/language >/dev/null 2>&1 || echo "E" | sudo tee /etc/wireguard/language >/dev/null 2>&1
+	[[ $LANGUAGE = 2 ]] && echo "C" | sudo tee /etc/wireguard/language >/dev/null 2>&1 || echo "E" | sudo tee /etc/wireguard/language >/dev/null 2>&1
 
 	# 自动刷直至成功（ warp bug，有时候获取不了ip地址）
 	green "\n ${L[12]}\n "
