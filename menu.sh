d6e2b94fc33a32eed40708e79c9d5f9b8b087564
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-05-27 08:05:03 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1613,7 +1613,6 @@ proxy(){
 
 	# 此处为处理 CentOS 7 安装 Glibc 2.28 之后 Running transaction test 不动的问题
 	if [ $GLIBC = 1 ]; then
-		export LANG="en_US.UTF-8";export LANGUAGE="en_US.UTF-8"
 		rm -rf /var/lib/rpm/__db*
 		yum clean all
 		rpm -v rebuilddb
