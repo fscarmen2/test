e9199a560e7568d8c59fc3d6fcadd3e75b8d31c9
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-12 14:52:18 +0800

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -84,6 +84,7 @@ green(){ echo -e "\033[32m\033[01m$1\033[0m"; }
 test_area=("广州电信" "广州联通" "广州移动")
 test_ip=("58.60.188.222" "210.21.196.6" "120.196.165.2")
 TEMP_FILE='ip.test'
+TEMP_FILE2='backtrace.test'
 
 SystemInfo_GetOSRelease() {
     if [ -f "/etc/centos-release" ]; then # CentOS
@@ -1614,13 +1615,17 @@ print_end_time() {
     echo " 时间          : $date_time"
 }
 
+backtrace() {
+  curl https://raw.githubusercontent.com/zhanghanyun/backtrace/main/install.sh -sSf | sh > $TEMP_FILE2
+}
+
 check_return() {
   [[ ! -e return.sh ]] && curl -qO https://raw.githubusercontent.com/spiritLHLS/ecs/main/return.sh
   chmod +x return.sh >/dev/null 2>&1
 
   green "依次测试电信，联通，移动经过的地区及线路，核心程序来由: ipip.net ，请知悉！" >> $TEMP_FILE
   for ((a=0;a<${#test_area[@]};a++)); do
-    green "\n${test_area[a]} ${test_ip[a]}" >> $TEMP_FILE
+    yellow "\n\n${test_area[a]} ${test_ip[a]}" >> $TEMP_FILE
     ./return.sh ${test_ip[a]} >> $TEMP_FILE
   done
 }
@@ -1631,7 +1636,7 @@ checksystem
 checkpython
 checkcurl
 checkspeedtest
-{ check_return; }&
+{ backtrace; check_return; }&
 SystemInfo_GetSystemBit
 if [ "${release}" == "centos" ]; then
     yum update > /dev/null 2>&1
@@ -1669,14 +1674,14 @@ echo ""
 echo ""
 ./dp
 echo "解锁油管，奈菲，迪士尼以上面为准，下面这三测的不准"
-echo "---------------流媒体解锁--感谢RegionRestrictionCheck开源-------------"
+echo "\n---------------流媒体解锁--感谢RegionRestrictionCheck开源-------------"
 echo " 以下为IPV4网络测试"
 Global_UnlockTest 4
 echo " 以下为IPV6网络测试"
 Global_UnlockTest 6
-echo "-----------------三网回程--感谢zhanghanyun/backtrace开源--------------"
-curl https://raw.githubusercontent.com/zhanghanyun/backtrace/main/install.sh -sSf | sh
-echo "--------------------回程路由--感谢fscarmen开源------------------------"
+echo "\n-----------------三网回程--感谢zhanghanyun/backtrace开源--------------"
+cat $TEMP_FILE2
+echo "\n--------------------回程路由--感谢fscarmen开源------------------------"
 cat $TEMP_FILE
 next
 print_end_time
@@ -1689,4 +1694,4 @@ rm -rf dp
 rm -rf nf
 rm -rf tubecheck
 rm -rf ecs.sh
-rm -rf $TEMP_FILE
+rm -rf $TEMP_FILE $TEMP_FILE2
