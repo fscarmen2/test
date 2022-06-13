c1a700d618108895b38a602bafe66a3c2465cabd
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-13 10:08:31 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 ip=$1
 TEMP_FILE='ip.temp'
-
-ARCHITECTURE="$(uname -m)"
-case $ARCHITECTURE in
-	x86_64 )  FILE=besttrace;;
-	aarch64 ) FILE=besttracearm;;
-	* ) echo -e "只支持 AMD64、ARM64 使用，问题反馈:[https://github.com/fscarmen/tools/issues]" && exit 1;;
-esac
+FILE=$(uname -m | sed "s/x86_64/besttrace/" | sed "s/aarch64/besttracearm/")
 
 [[ -z "$ip" ]] && echo -e "请填入上对端 IP" && exit 1
-echo -e "\n说明：测 VPS ——> 对端 经过的地区及线路，填本地IP就是测回程。"
+
 [[ ! -e "$FILE" ]] && wget -qN https://cdn.jsdelivr.net/gh/fscarmen/tools/besttrace/$FILE
 chmod +x "$FILE" >/dev/null 2>&1
  ./"$FILE" "$ip" -g cn > $TEMP_FILE
