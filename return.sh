d13a86fdffc6c4b82edc0112c31e48a04885182b
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-13 09:38:14 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 TEMP_FILE='ip.temp'
 
-check_dependencies(){ for c in $@; do
-type -p $c >/dev/null 2>&1 || (echo -e " 安装 \$c 中…… " && ${PACKAGE_INSTALL[b]} "$c") || (echo -e " 先升级软件库才能继续安装 \$c，时间较长，请耐心等待…… " && ${PACKAGE_UPDATE[b]} && ${PACKAGE_INSTALL[b]} "$c")
-! type -p $c >/dev/null 2>&1 && echo -e " 安装 \$c 失败，脚本中止，问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1; done; }
-
 ARCHITECTURE="$(uname -m)"
 case $ARCHITECTURE in
 x86_64 )  FILE=besttrace;;
@@ -36,13 +32,12 @@ done
 
 [[ -z $SYSTEM ]] && echo -e " 本脚本只支持 Debian、Ubuntu、CentOS 和 Alpine 系统,问题反馈:[https://github.com/fscarmen/warp_unlock/issues] " && exit 1
 
-check_dependencies curl sudo
 ip=$1
 echo -e "\n 本脚说明：测 VPS ——> 对端 经过的地区及线路，填本地IP就是测回程，核心程序来由: https://www.ipip.net/ ，请知悉！"
 [[ -z "$ip" || $ip = '[DESTINATION_IP]' ]] && reading "\n 请输入目的地 IP: " ip
 echo -e "\n 检测中，请稍等片刻。\n"
-[[ ! -e "$FILE" ]] && curl -sO https://cdn.jsdelivr.net/gh/fscarmen/tools/besttrace/$FILE
+[[ ! -e "$FILE" ]] && wget -N https://cdn.jsdelivr.net/gh/fscarmen/tools/besttrace/$FILE
 chmod +x "$FILE" >/dev/null 2>&1
-sudo ./"$FILE" "$ip" -g cn > $TEMP_FILE
-echo -e "$(cat $TEMP_FILE | sed "s/.*\*\(.*\)/\1/g" | sed "s/.*AS[0-9]*//g" | sed "/\*$/d;/^$/d;1d" | uniq | awk '{printf("%d.%s\n"),NR,$0}')"
+ ./"$FILE" "$ip" -g cn > $TEMP_FILE
+cat $TEMP_FILE | sed "s/.*\*\(.*\)/\1/g" | sed "s/.*AS[0-9]*//g" | sed "/\*$/d;/^$/d;1d" | uniq | awk '{printf("%d.%s\n"),NR,$0}'
 rm -f $TEMP_FILE $FILE
