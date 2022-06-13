334388513b03024bb4c8170919b49db157038593
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-13 09:51:55 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -5,7 +5,7 @@ ARCHITECTURE="$(uname -m)"
 case $ARCHITECTURE in
 x86_64 )  FILE=besttrace;;
 aarch64 ) FILE=besttracearm;;
-* ) echo -e " 只支持 AMD64、ARM64 使用，问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1;;
+* ) echo -e "只支持 AMD64、ARM64 使用，问题反馈:[https://github.com/fscarmen/tools/issues]" && exit 1;;
 esac
 
 # 多方式判断操作系统，试到有值为止。只支持 Debian 10/11、Ubuntu 18.04/20.04 或 CentOS 7/8 ,如非上述操作系统，退出脚本
@@ -30,12 +30,11 @@ for ((b=0; b<${#REGEX[@]}; b++)); do
 	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[b]} ]] && SYSTEM="${RELEASE[b]}" && break
 done
 
-[[ -z $SYSTEM ]] && echo -e " 本脚本只支持 Debian、Ubuntu、CentOS 和 Alpine 系统,问题反馈:[https://github.com/fscarmen/warp_unlock/issues] " && exit 1
+[[ -z $SYSTEM ]] && echo -e "本脚本只支持 Debian、Ubuntu、CentOS 和 Alpine 系统,问题反馈:[https://github.com/fscarmen/warp_unlock/issues]" && exit 1
 
 ip=$1
-echo -e "\n 本脚说明：测 VPS ——> 对端 经过的地区及线路，填本地IP就是测回程，核心程序来由: https://www.ipip.net/ ，请知悉！"
+echo -e "\n说明：测 VPS ——> 对端 经过的地区及线路，填本地IP就是测回程。"
 [[ -z "$ip" || $ip = '[DESTINATION_IP]' ]] && read -rp " 请输入目的地 IP: " ip
-echo -e "\n 检测中，请稍等片刻。\n"
 [[ ! -e "$FILE" ]] && wget -qN https://cdn.jsdelivr.net/gh/fscarmen/tools/besttrace/$FILE
 chmod +x "$FILE" >/dev/null 2>&1
  ./"$FILE" "$ip" -g cn > $TEMP_FILE
